39129ae61c883ca8f74e1d1b30a635271dc7f6c9
mitchell-banse-mscst <mf2861ys@my.southeastmn.edu>
2017-06-27 07:25:45 -0500

    Completed adding courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,16 +8,29 @@ $(function() {
 			addCourses('response', response.courses.completed);
 		}
 	});
-	function addCourses (courses) {
+	function addCourses(courses) {
 			
 		var $badges = $('#badges');
 			
 		courses.forEach(function(course) {
-			var course = $('<div />', {
+			var $course = $('<div />', {
 				'class': 'course'
 			}).appendTo($badges);
 				
-			//$('h3')
+			$('<h3 />', {
+				text: course.title
+			}).appendTo($course);
+			
+			$('<img />', {
+				src: course.badge
+			}).appendTo($course);
+			
+			$('<a />', {
+				'class': 'btn btn-primary',
+				target: '_blank',
+				href: course.url,
+				text: 'See Course'
+			}).appendTo($course);
 		})
 	}
 	
