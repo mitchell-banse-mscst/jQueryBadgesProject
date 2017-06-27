b3f6637f531def05f14faadbf679e0a298f655af
mitchell-banse-mscst <mf2861ys@my.southeastmn.edu>
2017-06-27 07:07:55 -0500

    Add completed courses
    Add completed courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,24 +1,24 @@
 $(function() {
 
-  $.ajax({
-	url: 'https://www.codeschool.com/users/2603825.json',
-	dataType: 'jsonp',
-	success: function (response) {
-		//handle response
-		addCourses('response', response.courses.completed);
-		
-		function addCourses (courses) {
-			
-			var $badges = $('#badges');
-			
-			courses.forEach(function(course) {
-				var course = $('<div_>', {
-					'class' : 'course'
-				}).appendTo($badges);
-				
-				$('h3')
-			})
+   $.ajax({
+		: 'https://www.codeschool.com/users/2603825.json',
+		dataType: 'jsonp',
+		success: function (response) {
+			//handle response
+			addCourses('response', response.courses.completed);
 		}
+	});
+	function addCourses (courses) {
+			
+		var $badges = $('#badges');
+			
+		courses.forEach(function(course) {
+			var course = $('<div />', {
+				'class': 'course'
+			}).appendTo($badges);
+				
+			//$('h3')
+		})
 	}
-  });
+	
 });
